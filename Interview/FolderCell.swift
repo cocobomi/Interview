55532805c6dd3bb416994d96dd0be93f5f85b470
cocobomi <ehdgus0424@naver.com>
2022-07-21 03:41:49 +0900

    카테고리 및 즐겨찾기 탭바 구현

---
+++ Interview/FolderCell.swift
@@ -0,0 +1,12 @@
+//
+//  FolderCell.swift
+//  Interview
+//
+//  Created by donghyeon on 2022/07/21.
+//
+
+import UIKit
+
+class FolderCell: UICollectionViewCell {
+    
+}
---
+++ Interview/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Interview
+//
+//  Created by donghyeon on 2022/07/21.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
